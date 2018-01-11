73fdf1b5451ac0e403f762c4963a94f8382186d9
Skalka <Skalka@SKALKA-PC>
2018-01-11 16:10:06 +0100

    kk

--- ConsoleApplication1/ConsoleApp1/Program.cs
+++ ConsoleApplication1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worldd!");
+            Console.WriteLine("Hello Worldds!");
         }
     }
 }
